abbe989241dc48d3a003a27e769cca2dbbb52382
ewhalgand <halgandewen44@gmail.com>
2025-04-04 09:39:49 +0200

    new commit

--- script/tabl.js
+++ script/tabl.js
@@ -1,20 +1,56 @@
-export const correctColors = ["blue", "red"];
+export const correctColors = ["blue", "blue", "orange", "black"];
 
 export const numberChoices = [
   {
-    "colors": ["Blue", "Red", "Purple", "Green"],
+    "colors": [
+      "Blue",
+      "Red",
+      "Purple",
+      "Green",
+      "Black",
+      "Gray",
+      "Orange",
+      "Pink",
+    ],
     "value": "firstChoice",
   },
   {
-    "colors": ["Blue", "Red", "Purple", "Green"],
+    "colors": [
+      "Blue",
+      "Red",
+      "Purple",
+      "Green",
+      "Black",
+      "Gray",
+      "Orange",
+      "Pink",
+    ],
     "value": "secondChoice",
   },
-  // {
-  //   "colors": ["Blue", "Red", "Purple", "Green"],
-  //   "value": "thirdChoice",
-  // },
-  // {
-  //   "colors": ["Blue", "Red", "Purple", "Green"],
-  //   "value": "fourthChoice",
-  // },
+  {
+    "colors": [
+      "Blue",
+      "Red",
+      "Purple",
+      "Green",
+      "Black",
+      "Gray",
+      "Orange",
+      "Pink",
+    ],
+    "value": "thirdChoice",
+  },
+  {
+    "colors": [
+      "Blue",
+      "Red",
+      "Purple",
+      "Green",
+      "Black",
+      "Gray",
+      "Orange",
+      "Pink",
+    ],
+    "value": "fourthChoice",
+  },
 ];
